43194c9ef1265e33d63efdc8deeaf9e8ff50f8bf
Francisco Nevitt Gonçalves <git@francisco.wiki>
2026-02-13 14:34:20 +0000

    Add `zig build test` step that runs skeleton test
    Builds and runs some SIMD tests for the available native CPU extensions.

--- build.zig
+++ build.zig
@@ -42,4 +42,32 @@ pub fn build(b: *std.Build) !void {
     b.installArtifact(lib);
 
     // TODO: Add contrib libs.
+
+    const skeleton_test = b.addExecutable(.{
+        .name = "skeleton_test",
+        .root_module = b.createModule(.{
+            .target = target,
+            .optimize = optimize,
+        }),
+    });
+    skeleton_test.linkLibrary(lib);
+    // We need these paths for `HWY_TARGET_INCLUDE`.
+    skeleton_test.addIncludePath(upstream.path(""));
+    skeleton_test.addIncludePath(upstream.path("hwy"));
+    const skeleton_test_flags = &.{
+        "-DHWY_IS_TEST=1",
+        "-DHWY_TEST_STANDALONE=1",
+    };
+    skeleton_test.addCSourceFiles(.{
+        .root = upstream.path("hwy"),
+        .files = &.{
+            "examples/skeleton.cc",
+            "examples/skeleton_test.cc",
+            "tests/test_util.cc",
+        },
+        .flags = skeleton_test_flags,
+    });
+    const skeleton_test_run = b.addRunArtifact(skeleton_test);
+    const test_step = b.step("test", "Run skeleton test");
+    test_step.dependOn(&skeleton_test_run.step);
 }
